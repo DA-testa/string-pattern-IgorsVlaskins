8a28073385f4bd2fc12fb5b22de0914609c8a8c5
IgorsVlaskins <125510437+IgorsVlaskins@users.noreply.github.com>
2023-04-18 00:00:06 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,44 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
+
+    input_type = input().strip()
+    if input_type == "F":
+        with open("tests/1", "r") as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+    else:
+        pattern = input().strip()
+        text = input().strip()
+
     # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return pattern, text
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
 
-    # and return an iterable variable
-    return [0]
+    result = []
+    p = 1000000007
+    x = 263
+    t_len = len(text)
+    p_len = len(pattern)
+    p_hash = sum(ord(pattern[i]) * pow(x, i, p) for i in range(p_len)) % p
+    t_hash = sum(ord(text[i]) * pow(x, i, p) for i in range(p_len)) % p
 
+    x_p = pow(x, p_len, p)
+    for i in range(t_len - p_len + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+p_len]:
+                result.append(i)
+        if i < t_len - p_len:
+            t_hash = (t_hash - ord(text[i]) * x_p) % p
+            t_hash = (t_hash * x + ord(text[i+p_len])) % p
+            t_hash = (t_hash + p) % p
 
-# this part launches the functions
+    return result
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
