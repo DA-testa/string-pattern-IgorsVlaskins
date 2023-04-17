507e9433a49fbf2b318eadb6ea8336424620f0bc
IgorsVlaskins <125510437+IgorsVlaskins@users.noreply.github.com>
2023-04-18 00:06:55 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,45 +1,32 @@
-import numpy as np
-
 def read_input():
-    choice = input().rstrip()
-    if choice == 'I':
+    input_type = input()
+    if "I" in input_type or "i" in input_type:
         pattern = input().rstrip()
         text = input().rstrip()
-    elif choice == 'F':
-        with open("tests/1", "r") as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
+    elif "F" in input_type or "f" in input_type:
+        file = "06"
+        if "a" not in file:
+            with open("tests/" + file, 'r')as f:
+                pattern = f.readline().rstrip()
+                text = f.readline().rstrip()
     return pattern, text
 
 def print_occurrences(output):
-    print(" ".join(map(str, output)))
-
-def poly_hash(s, prime, x):
-    ans = 0
-    for c in reversed(s):
-        ans = (ans * x + ord(c)) % prime
-    return ans
-
-def precompute_hashes(text, len_pattern, prime, x):
-    len_text = len(text)
-    H = np.zeros(len_text - len_pattern + 1, dtype=np.int64)
-    S = text[len_text - len_pattern:]
-    H[len_text - len_pattern] = poly_hash(S, prime, x)
-    y = 1
-    for i in range(len_pattern):
-        y = (y * x) % prime
-    for i in range(len_text - len_pattern - 1, -1, -1):
-        H[i] = (x * H[i + 1] + ord(text[i]) - y * ord(text[i + len_pattern])) % prime
-    return H
+    print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    prime = 1000000007
-    x = np.random.randint(1, prime)
-    len_pattern = len(pattern)
-    len_text = len(text)
-    pattern_hash = poly_hash(pattern, prime, x)
-    H = precompute_hashes(text, len_pattern, prime, x)
-    return [i for i in range(len_text - len_pattern + 1) if pattern_hash == H[i]]
+    prime = 1
+    pattern_len, text_len = len(pattern), len(text)
+    pattern_hash = sum(ord(pattern[i]) * pow(prime, i) for i in range(pattern_len))
+    text_hash = sum(ord(text[i]) * pow(prime, i) for i in range(pattern_len))
+    occurrences = []
+    for i in range(text_len - pattern_len + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+pattern_len]:
+                occurrences.append(i)
+        if i < text_len - pattern_len:
+            text_hash = (text_hash - ord(text[i])) / prime + ord(text[i+pattern_len]) * pow(prime, pattern_len-1)
+    return occurrences
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
